ad529f79b1d81fb6e768bea1d205c17f0ff66396
Stefan Bley <stefan.bley@saxsys.de>
2014-03-18 18:37:05 +0100

    campus.sql

---
+++ business/src/test/db/campus.sql
@@ -0,0 +1,140 @@
+CREATE DATABASE  IF NOT EXISTS `campus` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `campus`;
+-- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
+--
+-- Host: localhost    Database: campus
+-- ------------------------------------------------------
+-- Server version	5.5.28-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `reservation`
+--
+
+DROP TABLE IF EXISTS `reservation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reservation` (
+  `SLOT_ID` int(11) NOT NULL DEFAULT '0',
+  `USER_ID` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`USER_ID`,`SLOT_ID`),
+  KEY `SLOT_ID_idx` (`SLOT_ID`),
+  KEY `USER_ID_idx` (`USER_ID`),
+  CONSTRAINT `USER_FK` FOREIGN KEY (`USER_ID`) REFERENCES `user` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `SLOT_FK` FOREIGN KEY (`SLOT_ID`) REFERENCES `slot` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservation`
+--
+
+LOCK TABLES `reservation` WRITE;
+/*!40000 ALTER TABLE `reservation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reservation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `room`
+--
+
+DROP TABLE IF EXISTS `room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `room` (
+  `ID` int(11) NOT NULL,
+  `ROOMNUMBER` varchar(40) NOT NULL,
+  `CAPACITY` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ROOMNUMBER` (`ROOMNUMBER`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room`
+--
+
+LOCK TABLES `room` WRITE;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+INSERT INTO `room` VALUES (1,'FFP 302',20),(2,'BS UG',30);
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `slot`
+--
+
+DROP TABLE IF EXISTS `slot`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `slot` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `TITLE` varchar(255) NOT NULL,
+  `DESCRIPTION` varchar(255) DEFAULT NULL,
+  `STARTTIME` time NOT NULL,
+  `ENDTIME` time NOT NULL,
+  `ROOM_ID` int(11) NOT NULL,
+  `SPEAKER` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `ROOM_FK_idx` (`ROOM_ID`),
+  CONSTRAINT `ROOM_FK` FOREIGN KEY (`ROOM_ID`) REFERENCES `room` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `slot`
+--
+
+LOCK TABLES `slot` WRITE;
+/*!40000 ALTER TABLE `slot` DISABLE KEYS */;
+INSERT INTO `slot` VALUES (1,'Java 8 Lambdas','Workshop','09:30:00','10:30:00',1,'Stefan Saring'),(2,'Neo4J','Vortrag zu APIs und Cypher','09:30:00','12:00:00',2,'Matthias Baumgart'),(3,'JSF vs JavaScript','Vergleich','16:00:00','17:00:00',1,'Stefan Bley, Marco Dierenfeldt'),(5,'Horst','a','01:00:00','02:00:00',2,'q');
+/*!40000 ALTER TABLE `slot` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `ID` int(11) NOT NULL,
+  `USERNAME` varchar(50) NOT NULL,
+  `FIRSTNAME` varchar(50) NOT NULL,
+  `LASTNAME` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `USERNAME_UNIQUE` (`USERNAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'stefan.heinze','Stefan','Heinze'),(2,'marco.dierenfeldt','Marco','Dierenfeldt'),(3,'stefan.bley','Stefan','Bley');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-03-18 18:35:06
