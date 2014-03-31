5bb0a286fd378272bea0c8e8a925e1c55241e298
Matthias Baumgart <matthias.baumgart@saxsys.de>
2014-03-31 11:13:59 +0200

    [jsf] implements edit slot.

--- jsf/src/main/java/de/saxsys/campus/jsf/SlotBean.java
+++ jsf/src/main/java/de/saxsys/campus/jsf/SlotBean.java
@@ -1,13 +1,10 @@
 package de.saxsys.campus.jsf;
 
 import java.io.Serializable;
-import java.util.Date;
-import java.util.List;
 
-import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -15,11 +12,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.saxsys.campus.business.SlotManager;
-import de.saxsys.campus.domain.Room;
 import de.saxsys.campus.domain.Slot;
 
 @Named
-@ViewScoped
+@Stateless
 public class SlotBean implements Serializable {
 
 	private static final long serialVersionUID = -7322019173114315205L;
@@ -28,94 +24,26 @@ public class SlotBean implements Serializable {
 	@Inject
 	private SlotManager slotManager;
 
-	private String title;
-	private String description;
-	private Room room;
-	private Date startTime;
-	private Date endTime;
-	private int participants;
-	private String speaker;
+	private Slot slot;
 
-	private List<Room> rooms;
-
-	@PostConstruct
-	public void init() {
-		rooms = slotManager.allRooms();
+	public Slot getSlot() {
+		return slot;
 	}
 
-	public String getTitle() {
-		return title;
+	public void setSlot(Slot slot) {
+		this.slot = slot;
 	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Room getRoom() {
-		return room;
-	}
-
-	public void setRoom(Room room) {
-		this.room = room;
-	}
-
-	public Date getStartTime() {
-		return startTime;
-	}
-
-	public void setStartTime(Date startTime) {
-		this.startTime = startTime;
-	}
-
-	public Date getEndTime() {
-		return endTime;
-	}
-
-	public void setEndTime(Date endTime) {
-		this.endTime = endTime;
-	}
-
-	public int getParticipants() {
-		return participants;
-	}
-
-	public void setParticipants(int participants) {
-		this.participants = participants;
-	}
-
-	public String getSpeaker() {
-		return speaker;
-	}
-
-	public void setSpeaker(String speaker) {
-		this.speaker = speaker;
-	}
-
-	public List<Room> getRooms() {
-		return rooms;
-	}
-
+	
 	public void save() {
 		LOGGER.debug("Save slot");
 		try {
-			Slot slot = new Slot();
-			slot.setTitle(title);
-			slot.setDescription(description);
-			slot.setStarttime(startTime);
-			slot.setEndtime(endTime);
-			slot.setRoom(room);
-			slot.setSpeaker(speaker);
-			slotManager.addSlot(slot);
+			if (slot.getId() == null ) {
+				slotManager.addSlot(slot);			
+			} else {
+				slotManager.updateSlot(slot);
+			}
 
-			LOGGER.info("Slot {} saved.", title);
+			LOGGER.info("Slot {} saved.", slot.getTitle());
 			FacesContext.getCurrentInstance().addMessage(
 					null,
 					new FacesMessage(FacesMessage.SEVERITY_INFO, "Der Slot wurde gespeichert.",
@@ -129,4 +57,10 @@ public class SlotBean implements Serializable {
 							"Der Slot konnte nicht gespeichert werden.", e.getMessage()));
 		}
 	}
+
+
+
+
+	
+
 }
