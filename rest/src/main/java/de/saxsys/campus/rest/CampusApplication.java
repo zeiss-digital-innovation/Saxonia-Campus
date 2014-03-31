7f30d247f219138db69fdffa48edd0e39e46e545
Stefan Bley <stefan.bley@saxsys.de>
2014-03-31 18:41:24 +0200

    [rest] use GlassFish 4.0.1 and Jersey 2.5.1

--- rest/src/main/java/de/saxsys/campus/rest/CampusApplication.java
+++ rest/src/main/java/de/saxsys/campus/rest/CampusApplication.java
@@ -6,7 +6,6 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 
 import de.saxsys.campus.rest.auth.AuthenticationFilter;
-import de.saxsys.campus.rest.cdi.GlassfishHkCdiBinder;
 import de.saxsys.campus.rest.hal.HalBuilderMessageBodyReader;
 import de.saxsys.campus.rest.hal.HalBuilderMessageBodyWriter;
 
@@ -14,11 +13,10 @@ import de.saxsys.campus.rest.hal.HalBuilderMessageBodyWriter;
 public class CampusApplication extends ResourceConfig {
 
 	public CampusApplication() {
-		this.packages("de.saxsys.campus.rest.resource");
-		this.register(HalBuilderMessageBodyReader.class);
-		this.register(HalBuilderMessageBodyWriter.class);
-		this.register(RolesAllowedDynamicFeature.class);
-		this.register(AuthenticationFilter.class);
-		this.register(new GlassfishHkCdiBinder());
+		packages("de.saxsys.campus.rest.resource");
+		register(HalBuilderMessageBodyReader.class);
+		register(HalBuilderMessageBodyWriter.class);
+		register(RolesAllowedDynamicFeature.class);
+		register(AuthenticationFilter.class);
 	}
 }
