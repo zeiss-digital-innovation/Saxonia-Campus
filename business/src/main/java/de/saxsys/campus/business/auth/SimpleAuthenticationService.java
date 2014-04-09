1bc37a8c351cae9155a2dbe5e571588105998612
Matthias Baumgart <matthias@briefhansa.de>
2014-04-09 15:37:11 +0200

    [business] returns real user object after successful authentication.

--- business/src/main/java/de/saxsys/campus/business/auth/SimpleAuthenticationService.java
+++ business/src/main/java/de/saxsys/campus/business/auth/SimpleAuthenticationService.java
@@ -1,8 +1,10 @@
 package de.saxsys.campus.business.auth;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import de.saxsys.campus.business.UserManager;
 import de.saxsys.campus.domain.User;
 
 @Stateless
@@ -10,15 +12,18 @@ import de.saxsys.campus.domain.User;
 public class SimpleAuthenticationService implements AuthenticationService {
 
 	private static final String DEFAULT_PASSWORD = "campus";
+	
+	@Inject
+	private UserManager userManager;
 
 	@Override
 	public User authenticate(final String username, final String password)
 			throws AuthenticationException {
-		// username = password
 		if (!DEFAULT_PASSWORD.equals(password)) {
 			throw new AuthenticationException("Invalid password for user " + username);
 		}
-		return new User(username, null, null);
+		// TODO implement errorhandling
+		return userManager.findUser(username);
 	}
 
 }
