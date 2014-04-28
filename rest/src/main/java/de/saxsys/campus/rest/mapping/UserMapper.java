809283fba630e25326c58179a0c55f9277e8ef02
Stefan Bley <stefan.bley@saxsys.de>
2014-04-28 13:39:52 +0200

    [rest] UserResource with current user

--- rest/src/main/java/de/saxsys/campus/rest/mapping/UserMapper.java
+++ rest/src/main/java/de/saxsys/campus/rest/mapping/UserMapper.java
@@ -9,25 +9,20 @@ import javax.ws.rs.core.UriBuilder;
 import com.theoryinpractise.halbuilder.api.Representation;
 import com.theoryinpractise.halbuilder.api.RepresentationFactory;
 
-import de.saxsys.campus.rest.resource.SlotResource;
+import de.saxsys.campus.domain.User;
 import de.saxsys.campus.rest.resource.UserResource;
 
 @Singleton
-public class HomeMapper {
-
-	private static final String SLOTS = "slots";
-	private static final String CURRENTUSER = "currentUser";
+public class UserMapper {
 
 	@Inject
 	private RepresentationFactory representationFactory;
 
-	public Representation createRepresentation(URI baseUri) {
+	public Representation createRepresentation(URI baseUri, User user) {
 		return representationFactory
-				.newRepresentation(baseUri)
-				.withLink(SLOTS, UriBuilder.fromUri(baseUri).path(SlotResource.class).build())
-				.withLink(
-						CURRENTUSER,
+				.newRepresentation(
 						UriBuilder.fromUri(baseUri).path(UserResource.class).path("current")
-								.build());
+								.build()).withProperty("id", user.getId())
+				.withProperty("username", user.getUsername()).withProperty("role", user.getRole());
 	}
 }
