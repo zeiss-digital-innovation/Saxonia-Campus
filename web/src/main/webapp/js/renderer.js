1885e40da841faab3f9e920d575c76fd262b70d0
Marco Dierenfeldt <marco.dierenfeldt@saxsys.de>
2014-05-16 12:05:20 +0200

    Entfernen des Buchen Buttons wenn keine Freien Plätze verfügbar sind. Zusätzlich kleinere optisc he Tunings

--- web/src/main/webapp/js/renderer.js
+++ web/src/main/webapp/js/renderer.js
@@ -4,57 +4,65 @@
 
 var saxoniaCampusRenderer = {};
 
-saxoniaCampusRenderer.generateInnerSlot = function(slot){
-    var innerSlot = '<a id="' 
-          + slot.id + '_edit" class="edit_slot">'
-          + slot.title + '<p class="ui-li-aside">' + slot.starttime + " bis "
-          + slot.endtime + '</p></a>' + '<a class="delete_slot" id="' 
-          + slot.id + '_delete"></a>';
-  
+saxoniaCampusRenderer.generateInnerSlot = function(slot) {
+    var innerSlot = '<a id="'
+            + slot.id + '_edit" class="edit_slot">'
+            + slot.title + '<p class="ui-li-aside">' + slot.starttime + " bis "
+            + slot.endtime + '</p></a>' + '<a class="delete_slot" id="'
+            + slot.id + '_delete"></a>';
+
     return innerSlot;
 };
 
-saxoniaCampusRenderer.generateAdminViewSlot = function(slot){
-  var innerSlot = saxoniaCampusRenderer.generateInnerSlot(slot);
-  var slotHtml = '<li id="' + slot.id + '_slot">' + innerSlot + '</li>';
-  
+saxoniaCampusRenderer.generateAdminViewSlot = function(slot) {
+    var innerSlot = saxoniaCampusRenderer.generateInnerSlot(slot);
+    var slotHtml = '<li id="' + slot.id + '_slot">' + innerSlot + '</li>';
+
     return slotHtml;
 };
 
 saxoniaCampusRenderer.renderAdminViewSlot = function(slotListSelector, slot) {
     var adminViewSlotHtml = saxoniaCampusRenderer.generateAdminViewSlot(slot);
-    
+
     $(slotListSelector).append(adminViewSlotHtml);
 };
 
-saxoniaCampusRenderer.renderRoomOption = function(roomSelectSelector, room){
-    var option = '<option value="'+room.id+'">'+room.roomnumber+'</option>';
-    
+saxoniaCampusRenderer.renderRoomOption = function(roomSelectSelector, room) {
+    var option = '<option value="' + room.id + '">' + room.roomnumber + '</option>';
+
     $(roomSelectSelector).append(option);
 };
 
 saxoniaCampusRenderer.renderUserViewBookedSlot = function(slotListSelector, slot) {
-    var slotHtml = '<li id="' + slot.id + '_slot"><a>' + slot.title + '<p class="ui-li-aside">'
+    var slotHtml = '<li id="' + slot.id + '_slot"><a><span class="booked_slot_title">' + slot.title + '</span><p class="ui-li-aside">'
             + slot.starttime + " bis " + slot.endtime + '</p></a><a id="' + slot.id
             + '_delete_slot" class="delete_slot"></a></li>';
     $(slotListSelector).append(slotHtml);
 };
 
 saxoniaCampusRenderer.renderUserViewDetailSlot = function(slotListSelector, slot) {
+    var freeCapacity = (slot.capacity - slot.participants);
     var slotHtml = '<div data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u">'
             + '<h3>' + slot.title + ' - ' + slot.starttime + ' bis ' + slot.endtime + '</h3>'
             + '<table>'
-            + '<tr><td colspan="2">Beschreibung: </td>'
-            + '<td><button class="book_slot_btn" id="' + slot.id
-            + '_book_btn" data-mini="true" data-inline="true">Buchen</button></td>'
-            + '</tr>'
+            + '<tr><td colspan="2">Beschreibung: </td>';
+
+    //Buchen-Button nur rendern, wenn freie Plätze vorhanden sind
+    if (freeCapacity > 0) {
+        slotHtml = slotHtml + '<td><button class="book_slot_btn" id="' + slot.id
+                + '_book_btn" data-mini="true" data-inline="true">Buchen</button></td>';
+    } else {
+        slotHtml = slotHtml + '<td></td>';
+    }
+
+    slotHtml = slotHtml + '</tr>'
             + '<tr><td colspan="2">' + slot.description + '</td><td></td></tr>'
-            + '<tr><td>Raum:<br>'+slot.room+'</td>'
-            + '<td>Speaker:<br>'+slot.speaker+'</td>'
-            + '<td>Freie Plätze: <span id="'+slot.id+'_free">' + (slot.capacity - slot.participants) + '</span></td>'
+            + '<tr><td>Raum:<br>' + slot.room + '</td>'
+            + '<td>Speaker:<br>' + slot.speaker + '</td>'
+            + '<td>Freie Plätze:<br><span id="' + slot.id + '_free">' + freeCapacity + '</span></td>'
             + '</tr>'
             + '</table>'
             + '</div>';
-    
+
     $(slotListSelector).append(slotHtml);
 };
